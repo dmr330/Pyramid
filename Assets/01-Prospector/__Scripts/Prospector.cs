ed593fa216bd40ddb55b1b3dc6f28b66e0b47a76
dmr330 <dmr330@scarletmail.rutgers.edu>
2024-03-24 23:31:07 -0400

    Working With CardProspector
    Step Complete

--- Assets/01-Prospector/__Scripts/Prospector.cs
+++ Assets/01-Prospector/__Scripts/Prospector.cs
@@ -10,15 +10,16 @@ public class Prospector : MonoBehaviour {
 	static public Prospector 	S;
 
 	[Header("Set in Inspector")]
-	public TextAsset			deckXML;
+	public TextAsset deckXML;
 	public TextAsset layoutXML;
 
 
 	[Header("Set Dynamically")]
-	public Deck					deck;
+	public Deck	deck;
 	public Layout layout;
+    public List<CardProspector> drawPile;
 
-	void Awake(){
+    void Awake(){
 		S = this;
 	}
 
@@ -28,8 +29,19 @@ public class Prospector : MonoBehaviour {
 
         layout = GetComponent<Layout>(); // Get the Layout component
         layout.ReadLayout(layoutXML.text); // Pass LayoutXML to it
+
+
+        drawPile = ConvertListCardsToListCardProspectors(deck.cards);
     }
-
-
-
+    List<CardProspector> ConvertListCardsToListCardProspectors(List<Card> lCD)
+    {
+        List<CardProspector> lCP = new List<CardProspector>();
+        CardProspector tCP;
+        foreach (Card tCD in lCD)
+        {
+            tCP = tCD as CardProspector;
+            lCP.Add(tCP);
+        }
+        return (lCP);
+    }
 }
