14df538f46fa7b8d23db50c6214e951a8e435f07
Ethan-Lee-28 <ekl51@scarletmail.rutgers.edu>
2024-03-28 20:40:42 -0400

    updated scenes

---
+++ Assets/01-Prospector/__Scripts/LoadNewScene.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadNewScene : MonoBehaviour
+{
+
+    public void LoadMod3Scene(string sceneToLoad)
+    {
+        SceneManager.LoadScene(sceneToLoad);
+    }
+
+}
